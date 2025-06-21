99b7c8f904eb2c366d8b42a4a7669d44590a61b1
Matthew Keaton <keatoncmts43@yahoo.com>
2025-06-21 18:13:16 -0400

    Add files via upload

---
+++ ProjectTwo_WeightTrackingApp/MyWeightTrackingApp/app/src/main/java/com/example/myweighttrackingapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.myweighttrackingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+// Create class to extend MainActivity
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
